00e05ec4c384ca243a2686c5b3dc5f6d23ed4c3c
Aniket <aniketbly@gmail.com>
2025-05-05 21:30:34 +0530

    Initial Commit

---
+++ WordOccurence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordOccurence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Shivani Mishra";
+            int count = 0;
+            char[] arr = str.ToLower().ToArray();
+            
+            for(int i=0;i<arr.Length;i++)
+            {
+                count = 0;
+                for (int j=0;j<arr.Length;j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine("Char:- " + arr[i] + "-Count:-" + count);
+            }
+            
+
+        }
+    }
+}
